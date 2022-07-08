b1e9e3a064419b13935681fe5f52d47a6d344de1
sajidahmed167 <samee_ahmed247@yahoo.com>
2022-07-08 11:40:51 -0400

    v1.0
    sessions created

---
+++ OrderBot.tests/OrderBotTest.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using Xunit;
+using OrderBot;
+
+namespace OrderBot.tests
+{
+    public class OrderBotTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        [Fact]
+        public void TestWelcome()
+        {
+            Session oSession = new Session("12345");
+            String sInput = oSession.OnMessage("hello");
+            Assert.True(sInput.Contains("Welcome"));
+        }
+        [Fact]
+        public void TestShawarama()
+        {
+            Session oSession = new Session("12345");
+            String sInput = oSession.OnMessage("hello");
+            Assert.True(sInput.ToLower().Contains("shawarama"));
+        }
+        [Fact]
+        public void TestSize()
+        {
+            Session oSession = new Session("12345");
+            String sInput = oSession.OnMessage("hello");
+            Assert.True(sInput.ToLower().Contains("size"));
+        }
+        [Fact]
+        public void TestLarge()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            String sInput = oSession.OnMessage("large");
+            Assert.True(sInput.ToLower().Contains("protein"));
+            Assert.True(sInput.ToLower().Contains("large"));
+        }
+        [Fact]
+        public void TestChicken()
+        {
+            string sPath = DB.GetConnectionString();
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            oSession.OnMessage("large");
+            String sInput = oSession.OnMessage("chicken");
+            Assert.True(sInput.ToLower().Contains("toppings"));
+            Assert.True(sInput.ToLower().Contains("large"));
+            Assert.True(sInput.ToLower().Contains("chicken"));
+        }
+    }
+}
---
+++ OrderBot/Session.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OrderBot
+{
+    public class Session
+    {
+        private enum State
+        {
+            WELCOMING, SYMPTOMS, DATE, CONFIRM
+        }
+
+        private State nCur = State.WELCOMING;
+        private Order oOrder;
+
+        public Session(string sPhone){
+            this.oOrder = new Order();
+            this.oOrder.Phone = sPhone;
+        }
+
+        public String OnMessage(String sInMessage)
+        {
+            String sMessage = "Welcome to Flu Fighters! Please enter your First name and Mobile number? (example: John Doe,+18888888888)";
+            switch (this.nCur)
+            {
+                case State.WELCOMING:
+                    this.nCur = State.SYMPTOMS;
+                    break;
+                case State.SYMPTOMS:
+                    this.oOrder.Size = sInMessage;
+                    this.oOrder.Save();
+                    sMessage = "What are the Symptoms for your visit, for multiple symptoms write each with coma in between? (Example: Fever,Coughing,Backaches)";
+                    this.nCur = State.DATE;
+                    break;
+                case State.DATE:
+                    string sProtein = sInMessage;
+                    sMessage = "What date are you available to visit us? (Example: 8 July 2022)";
+                    this.nCur = State.CONFIRM;
+                    this.oOrder.Save();
+                    break;
+                case State.CONFIRM:
+                    string sCOnfirm = sInMessage;
+                    sMessage = "Thank for booking an appointment";
+                    this.nCur = State.WELCOMING;
+                    this.oOrder.Save();
+                    break;
+                    
+
+            }
+            System.Diagnostics.Debug.WriteLine(sMessage);
+            return sMessage;
+        }
+
+    }
+}
